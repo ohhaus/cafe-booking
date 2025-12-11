d881ffee167fa0964397f2afb29b3699a60e565c
Никита Ефремчев <efremchef@yandex.ru>
2025-12-11 23:40:01 +0300

    feat(cafes/models): Создана модель Cafe
    Создана модель Cafe, выполнена связь между сущностями Cafe и User. Константы вынесены в файл config.py

---
+++ src/cafes/models.py
@@ -0,0 +1,81 @@
+from typing import Optional
+import uuid
+
+from sqlachemy import ForeignKey, String, Table, and_
+from sqlalchemy.dialects.postgres import UUID
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from src.config import (
+    MAX_ADRESS_LENGTH,
+    MAX_DESCRIPTION_LENGTH,
+    MAX_NAME_LENGTH,
+    MAX_PHONE_LENGTH,
+)
+from src.database import Base
+from src.photos.models import Photo
+from src.users.models import User, UserRole
+
+
+cafes_managers = Table(
+    'cafes_managers',
+    Base.metadata,
+    mapped_column(
+        'cafe_id',
+        UUID(as_uuid=True),
+        ForeignKey('cafe.id', ondelete='CASADE'),
+        primary_key=True,
+    ),
+    mapped_column(
+        'user_id',
+        UUID(as_uuid=True),
+        ForeignKey('user.id', ondelete='CASADE'),
+        primary_key=True,
+    ),
+)
+
+
+class Cafe(Base):
+    """Модель кафе."""
+
+    name: Mapped[str] = mapped_column(
+        String(MAX_NAME_LENGTH),
+        nullable=False,
+    )
+    adress: Mapped[str] = mapped_column(
+        String(MAX_ADRESS_LENGTH),
+        nullable=False,
+    )
+    phone: Mapped[str] = mapped_column(
+        String(MAX_PHONE_LENGTH),
+        nullable=False,
+    )
+    description: Mapped[str] = mapped_column(
+        String(MAX_DESCRIPTION_LENGTH),
+        nullable=True,
+    )
+    manager_id: Mapped[list[User]] = relationship(
+        'User',
+        secondary=cafes_managers,
+        primaryjoin=id == cafes_managers.c.cafe_id,
+        secondaryjoin=and_(
+            User.id == cafes_managers.c.user_id,
+            User.role == UserRole.MANAGER,
+        ),
+        viewonly=True,
+        overlaps='managed_cafes, cafes',
+    )
+    photo_id: Mapped[Optional[uuid:UUID]] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey('photo.id', ondelete='SET NULL'),
+        unique=True,
+        nullable=True,
+    )
+
+    photo: Mapped[Optional[Photo]] = relationship(
+        back_populates='cafe',
+        uselist=False,
+    )
--- src/config.py
+++ src/config.py
@@ -2,6 +2,12 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+MAX_NAME_LENGTH = 100
+MAX_ADRESS_LENGTH = 250
+MAX_PHONE_LENGTH = 32
+MAX_DESCRIPTION_LENGTH = 500
+
+
 class DatabaseSettings(BaseSettings):
     """Настройки базы данных."""
 
