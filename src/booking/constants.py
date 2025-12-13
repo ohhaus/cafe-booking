c2a470ce342e9b150ff26b4757eb71672772d3f2
Konstantin-Kleinikov <konstantinkleinikov@gmail.com>
2025-12-13 21:34:34 +0300

    feat: созданы модели booking и константы

---
+++ src/booking/constants.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+
+MAX_NOTES_LENGTH = 255
+
+
+class BookingStatus(IntEnum):
+    """Статус бронирования."""
+
+    CREATED = 0
+    ACTIVE = 1
+    COMPLETED = 2
+    CANCELED = 3
