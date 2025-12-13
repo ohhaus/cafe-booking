9fcfc3a09801ddc2799bfdc84ad7bac437e3d039
Vladimir Ignatev <ycherepaha@yandex.ru>
2025-12-13 14:54:44 +0300

    база под модель Блюда, убрал связь с Photo

--- src/dishes/models.py
+++ src/dishes/models.py
@@ -3,63 +3,50 @@ from decimal import Decimal
 from typing import Optional
 import uuid
 
-from sqlachemy import ForeignKey, Numeric, String
-from sqlalchemy.dialects.postgres import UUID
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    # relationship,
-)
+from sqlalchemy import Column, ForeignKey, String, Table, Numeric
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.config import (
     MAX_DESCRIPTION_LENGTH,
     MAX_NAME_LENGTH,
 )
 from src.database import Base
-from src.cafes.models import Cafe
-# from src.photos.models import Photo
-# from src.users.models import User, UserRole
 
-
-# cafes_managers = Table(
-#     'cafes_managers',
-#     Base.metadata,
-#     mapped_column(
-#         'cafe_id',
-#         UUID(as_uuid=True),
-#         ForeignKey('cafe.id', ondelete='CASADE'),
-#         primary_key=True,
-#     ),
-#     mapped_column(
-#         'user_id',
-#         UUID(as_uuid=True),
-#         ForeignKey('user.id', ondelete='CASADE'),
-#         primary_key=True,
-#     ),
-# )
+# Промежуточная таблица для связи между блюдами и кафе
+dishes_cafes = Table(
+    'dishes_cafes',
+    Base.metadata,
+    Column(
+        'dish_id',
+        UUID(as_uuid=True),
+        ForeignKey('dish.id', ondelete='CASCADE'),
+        primary_key=True,
+    ),
+    Column(
+        'cafe_id',
+        UUID(as_uuid=True),
+        ForeignKey('cafe.id', ondelete='CASCADE'),
+        primary_key=True,
+    ),
+)
 
 
 class Dish(Base):
-    """Модель блюда."""
+    """Модель блюда в кафе."""
 
     name: Mapped[str] = mapped_column(
         String(MAX_NAME_LENGTH),
         nullable=False,
-    )
-    description: Mapped[str] = mapped_column(
-        String(MAX_DESCRIPTION_LENGTH),
-        nulable=True,
-    )
-    cafes_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('cafe.id', ondelete='CASCADE'),
-        nullable=False,
-        index=True,
-    )
-    photo_id: Mapped[Optional[uuid:UUID]] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('photo.id', ondelete='SET NULL'),
         unique=True,
+    )
+    description: Mapped[Optional[str]] = mapped_column(
+        String(MAX_DESCRIPTION_LENGTH),
+        nullable=True,
+    )
+    # todo: добавить связь с фото/медиа в будущем
+    photo_id: Mapped[Optional[uuid.UUID]] = mapped_column(
+        UUID(as_uuid=True),
         nullable=True,
     )
     price: Mapped[Decimal] = mapped_column(
