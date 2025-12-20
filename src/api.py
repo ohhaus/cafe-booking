d745fc8d1977e78489cd3f20e6b16c8b68e3572e
Vladimir Ignatev <ycherepaha@yandex.ru>
2025-12-20 23:30:04 +0300

    оставил префикс users у роутера только во Views

--- src/api.py
+++ src/api.py
@@ -8,5 +8,5 @@ from src.users.views import router as user_router
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
-main_router.include_router(user_router, prefix='/users')
+main_router.include_router(user_router)
 main_router.include_router(booking_router)
