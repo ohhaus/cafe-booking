c1f118076b7fce470d4ffaa14140b725fce3f1b3
Vladimir Ignatev <ycherepaha@yandex.ru>
2025-12-21 02:44:10 +0300

    Исправление аутентификации

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.responces import LOGIN_RESPONSES
+from src.config import settings
 from src.database.sessions import get_async_session
 from src.users.schemas import LoginForm, Token
 from src.users.security import create_access_token, verify_password
@@ -27,7 +30,7 @@ async def login(
         session: AsyncSession = Depends(get_async_session),
         ) -> Token:
     """Возвращает токен для последующей авторизации пользователя."""
-    user = await user_crud.get_by_username(auth_data.username, session)
+    user = await user_crud.get_by_username(auth_data.login, session)
 
     if not user or not verify_password(auth_data.password,
                                        user.hashed_password):
@@ -36,6 +39,13 @@ async def login(
             detail='Неверные имя пользователя или пароль',
         )
 
-    access_token = create_access_token(data={"sub": user.username})
+    expires_delta = timedelta(
+        minutes=settings.auth.access_token_expire_minutes,
+        )
+
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=expires_delta,
+        )
 
     return Token(access_token=access_token, token_type="bearer")
