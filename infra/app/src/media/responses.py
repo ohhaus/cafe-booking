e90cd68086379bd282cd0a3b24d1e9242dc0fbc4
ohhaus <ohhaus84@gmail.com>
2026-01-07 13:28:49 +0300

    fix: исправил gitingnore, теперь media обрабатывается нормально

---
+++ infra/app/src/media/responses.py
@@ -0,0 +1,10 @@
+from src.cafes.responses import CREATE_RESPONSES, GET_BY_ID_RESPONSES
+from src.common.responses import (
+    media_get_by_id_responses,
+    media_post_responses,
+)
+from src.media.schemas import MediaInfo
+
+
+CREATE_RESPONSES = media_post_responses(MediaInfo)
+GET_BY_ID_RESPONSES = media_get_by_id_responses()
